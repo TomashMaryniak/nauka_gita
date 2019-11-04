7b846c868d8a0b14ac33d020f21feb4a850fd636
tomashmaryniak <tomashmary@user.noreply.github.com>
2019-11-04 22:39:27 +0100

    pierwsze poty python

---
+++ main.py
@@ -0,0 +1,10 @@
+marka = 'Pegot'
+ilosc_drzwi = 5
+pojemnosc = 1.5
+marka_upper = marka.upper()
+print (marka_upper)
+marka_down = marka.lower()
+print marka_down
+print marka[1:-1]
+print marka[2:-1]
+print()
